499019a55b50037c29cb72ca2e2b10fa16f73ac9
AleixTP23219 <ateresp17@alumnes.ilerna.com>
2026-03-10 15:55:46 +0100

    comentaris i clients creats

--- src/src/Client.java
+++ src/src/Client.java
@@ -4,20 +4,25 @@ import java.net.*; // Classes per xarxes
 public class Client {
     public static void main(String[] args) {
         try {
+            //Conecta al servidor
             Socket connexio = new Socket("127.0.0.1", 5000);
             System.out.println("Connectat al servidor");
 
+            //Escriptor per enviar el text al servidor
             PrintWriter escriptor = new PrintWriter(
                     connexio.getOutputStream(), true);
 
             escriptor.println("Hola patatilles");
             System.out.println("Missatge enviat: 'Me encante que te encanti entrar al servidor'");
 
+            //Lector per rebre el missatge del servidor
             BufferedReader lector = new BufferedReader(new InputStreamReader(connexio.getInputStream()));
 
+            //Llegeix la respostadel servidor
             String resposta = lector.readLine();
             System.out.println("Servidor respon: '" + resposta + "'");
 
+            //Es tanque per a no gastar mes recursos, es podria deixar obert pero seria inutil
             connexio.close();
             System.out.println("Connexió tancada.");
 
